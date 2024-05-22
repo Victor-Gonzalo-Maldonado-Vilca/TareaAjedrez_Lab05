920fb3c646e182f193c736536a4f9e4435e1e3c6
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-21 22:16:33 -0500

    Agregando fichas blancas y graficandolas

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -3,4 +3,5 @@ from chessPictures import *
 fila = square.join(square.negative()).horizontalRepeat(4)
 fichas = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)
 peones = pawn.horizontalRepeat(8)
-draw(fila.under(peones))
+fichasBlancas = fila.negative().under(fichas).up(fila.under(peones)) 
+draw(fila.under(fichasBlancas))
