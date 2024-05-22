d61d1be2b37f2e1441084306699ad511b68c2b39
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-21 21:16:30 -0500

    Haciendo prueba del funcionamiento de la modificación del métodod horizontalRepeat()

--- probando.py
+++ probando.py
@@ -1,4 +1,4 @@
 from chessPictures import *
 from interpreter import draw
-x =  square.under(rock.negative()).join(square.negative().under(king)).horizontalRepeat(4)
+x =  square.under(rock.negative()).up(square.negative().under(king)).horizontalRepeat(4)
 draw(x)
