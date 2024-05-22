2387280a041d9276a7e70774ada0ba253bf00995
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-21 22:09:30 -0500

    Implementando fichas y supoerponerlas en los casilleros

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -1,4 +1,5 @@
 from interpreter import draw
 from chessPictures import *
 fila = square.join(square.negative()).horizontalRepeat(4)
-draw(fila)
+fichas = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)
+draw(fila.under(fichas))
