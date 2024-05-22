c40c4d626eaa2fa38b652d1e2d42a87bb1f6c62b
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-05-21 22:11:14 -0500

    Agregando peones al ejercicio 7 (G), y superponerlos en los casilleros

--- Ejercicio2g.py
+++ Ejercicio2g.py
@@ -2,4 +2,5 @@ from interpreter import draw
 from chessPictures import *
 fila = square.join(square.negative()).horizontalRepeat(4)
 fichas = rock.join(knight).join(bishop).join(queen).join(king).join(bishop).join(knight).join(rock)
-draw(fila.under(fichas))
+peones = pawn.horizontalRepeat(8)
+draw(fila.under(peones))
